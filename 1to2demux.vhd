dd0e15415f7e4fc1810a548b03ed2cf0871b24d3
Debadrita Mondal <111679707+mondaldebadrita03@users.noreply.github.com>
2024-05-25 02:39:49 +0530

    Update 1to2demux.vhd

--- 1to2demux.vhd
+++ 1to2demux.vhd
@@ -3,11 +3,12 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 entity 1to2demux is
-    Port ( I : in  STD_LOGIC_VECTOR (7 downto 0);
+    Port ( I : in  STD_LOGIC;
            S : in  STD_LOGIC;
            Y : out  STD_LOGIC_VECTOR (1 downto 0));
 end 1to2demux;
 architecture Behavioral of 1to2demux is
 begin
-
+Y(0)<=not(S) and I;
+Y(1)<=S and I;    
 end Behavioral;
