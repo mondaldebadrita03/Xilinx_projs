41e3288584e216ab9bfa5395825d1993617694c8
Debadrita Mondal <111679707+mondaldebadrita03@users.noreply.github.com>
2024-05-25 00:56:02 +0530

    Update 2to1encoder.vhd

--- 2to1encoder.vhd
+++ 2to1encoder.vhd
@@ -3,8 +3,8 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 entity 2to1encoder is
-    Port ( I : in  STD_LOGIC_VECTOR (1 downto 0));
-           Y : out  STD_LOGIC;
+    Port ( I : in  STD_LOGIC_VECTOR (1 downto 0);
+           Y : out  STD_LOGIC);
 end 2to1encoder;
 architecture Behavioral of 2to1encoder is
 begin
