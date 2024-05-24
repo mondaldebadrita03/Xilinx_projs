27bdba9c32098b463e6582307129393709d36dae
Debadrita Mondal <111679707+mondaldebadrita03@users.noreply.github.com>
2024-05-25 00:49:50 +0530

    Create 2to1encoder.vhd

---
+++ 2to1encoder.vhd
@@ -0,0 +1,12 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+entity 2to1encoder is
+    Port ( I : in  STD_LOGIC_VECTOR (1 downto 0));
+           Y : out  STD_LOGIC;
+end 2to1encoder;
+architecture Behavioral of 2to1encoder is
+begin
+Y<=I(1) or '0'; 
+end Behavioral;
