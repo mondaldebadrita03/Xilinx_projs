c78e629d13893f26a91382d380eacb49284b2a91
Debadrita Mondal <111679707+mondaldebadrita03@users.noreply.github.com>
2024-05-24 12:32:15 +0530

    Update and_gate.vhd

--- and_gate.vhd
+++ and_gate.vhd
@@ -7,12 +7,8 @@ entity and_gate is
            b : in  STD_LOGIC;
            c : out  STD_LOGIC);
 end and_gate;
-
 architecture Behavioral of and_gate is
-
 begin
-
 c<=a and b;
-
 end Behavioral;
 
