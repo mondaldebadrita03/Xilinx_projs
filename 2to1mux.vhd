e36dd54ac6d4c0a003e09d4a6b1de40f208833f4
Debadrita Mondal <111679707+mondaldebadrita03@users.noreply.github.com>
2024-05-25 01:40:23 +0530

    Update 2to1mux.vhd

--- 2to1mux.vhd
+++ 2to1mux.vhd
@@ -1,1 +1,13 @@
-
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+entity 2to1mux is
+    Port ( I : in  STD_LOGIC_VECTOR (1 downto 0);
+           S : in  STD_LOGIC;
+           Y : out  STD_LOGIC);
+end 2to1mux;
+architecture Behavioral of 2to1mux is
+begin
+Y<= (not(S) and I(0)) or (S and I(1));
+end Behavioral;
