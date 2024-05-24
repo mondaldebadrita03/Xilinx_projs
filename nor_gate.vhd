5267a6d1aaeb1101e6a4663920aacbc707f8b3a1
Debadrita Mondal <111679707+mondaldebadrita03@users.noreply.github.com>
2024-05-24 12:44:46 +0530

    Create nor_gate.vhd

---
+++ nor_gate.vhd
@@ -0,0 +1,13 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+entity nor_gate is
+    Port ( a : in  STD_LOGIC;
+           b : in  STD_LOGIC;
+           c : out  STD_LOGIC);
+end nor_gate;
+architecture Behavioral of nor_gate is
+begin
+c<=not(a or b);
+end Behavioral;
